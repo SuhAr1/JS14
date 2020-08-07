d915d08531fd8e69eb20ec3aaf5bc4720ceeb37f
SuhAr1 <ar.sukhomlinov@gmail.com>
2020-08-07 13:19:26 +0300

    Дописал код js

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ alert('hallo');
 console.log('Вывел текст');
 
 const money = 10;
+console.log('money: ', money);
 const income = 15;
+console.log('income: ', income);
 const addExpenses = 19;
+console.log('addExpenses: ', addExpenses);
 const deposit = 100;
+console.log('deposit: ', deposit);
 const mission = 50;
+console.log('mission: ', mission);
 const period = 12;
+console.log('period: ', period);
