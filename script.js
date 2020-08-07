35c6eee7af79fc320d582b54bb85c1283791498e
SuhAr1 <ar.sukhomlinov@gmail.com>
2020-08-07 14:49:35 +0300

    Исправил код js

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+
 alert('hallo');
 console.log('Вывел много много текста');
-
-const money = 20;
-console.log('money: ', money);
-const income = 16;
-console.log('income: ', income);
-const addExpenses = 14;
-console.log('addExpenses: ', addExpenses);
-const deposit = 100;
-console.log('deposit: ', deposit);
-const mission = 60;
-console.log('mission: ', mission);
-const period = 24;
-console.log('period: ', period);
