4840874ed2c98939825f63ad60aa3d5ce8137927
SuhAr1 <ar.sukhomlinov@gmail.com>
2020-08-09 00:14:05 +0300

    Добавил много js кода

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 200000;
+let income = 'фриланс';
+let addExpenses ='интернет, такси, коммуналка';
+let deposit = true;
+let mission = 5000000;
+let period = 12;
+let budgetDay = money/30;
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+console.log(typeof deposit);
+console.log(typeof money);
+console.log(typeof income);
+console.log('Период равен', period, 'месяцев');
+console.log('Цель заработать', mission, 'рублей');
+console.log('budgetDay: ', budgetDay);
 
 alert('hallo');
 console.log('Вывел много много текста');
